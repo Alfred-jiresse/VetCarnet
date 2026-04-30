c78a9b20d22ddea41b427dbbd8c93e4aafb6d55f
Alfred-jiresse <jiressealfred067@gmail.com>
2026-04-30 18:20:37 +0100

    adding the other configurations files

---
+++ app/build/generated/source/buildConfig/debug/com/vetcarnet/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.vetcarnet;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.vetcarnet";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRleHl4d3piaWdleHppeGRvd3JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcyMjQzODIsImV4cCI6MjA5MjgwMDM4Mn0.nfLFcTYPfYliyo9Np4yrVeG4zI4NSAlIbGuNLom9SyQ";
+  // Field from default config.
+  public static final String SUPABASE_URL = "https://texyxwzbigexzixdowrs.supabase.co";
+}
